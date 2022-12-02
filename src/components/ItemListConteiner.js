67693146960c45e3e15205bc49cad2cc3566416a
Emiliano Carod <emicarod@gmail.com>
2022-12-01 21:45:47 -0300

    primeros-pasos

---
+++ src/components/ItemListConteiner.js
@@ -0,0 +1,12 @@
+import Cards from "./cards";
+
+const ItemListConteiner = () => {
+    return <div>
+        Home
+        <Cards/>
+        <Cards/>
+        <Cards/>
+    </div>;
+};
+
+export default ItemListConteiner;
